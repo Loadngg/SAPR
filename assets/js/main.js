7ebe69fad24d215b5e872e6f4a5748c3b6f3ebfc
Loadngg <averinj@mail.ru>
2021-10-27 23:52:41 +0300

    Версия 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// slider
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        dots:false,
+        infinity: true,
+		fade: true,
+  		cssEase: 'linear',
+        autoplay:true,
+        autoplaySpeed:2000,
+    });
+});
