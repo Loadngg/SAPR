ffe08fcc64ba6abb5d0c380a5d07721c8ab55ff4
Loadngg <averinj@mail.ru>
2021-11-23 19:30:25 +0300

    убран блокиратор

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,26 +11,12 @@ $(document).ready(function(){
     });
 });
 
-// Disable scrolling
-function disableScroll(){
-    $('body').bind('touchmove', function(e){e.preventDefault()})
-    var x=window.scrollX;
-    var y=window.scrollY;
-    window.onscroll=function(){window.scrollTo(x, y);};
-}
-
-function enableScroll(){
-    $('body').unbind('touchmove')
-    window.onscroll=function(){};
-}
-
 // overflow
 let currentOverlayId = null;
 
 function showOverflow(blockId) {
     currBlockId = blockId;
     let block = document.getElementById(currBlockId);
-    disableScroll();
     block.classList.remove('hidden'); 
     setTimeout(function(){
         block.classList.remove('transparent')
@@ -42,7 +28,6 @@ function hideOverflow() {
         return;
     let block = document.getElementById(currBlockId);
     let iframe = block.firstElementChild.firstElementChild;
-    enableScroll();
     block.classList.add('transparent'); 
     setTimeout(function(){
         block.classList.add('hidden')
