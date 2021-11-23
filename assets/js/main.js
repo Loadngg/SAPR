57ac1c1f60a693ac063251ed643183fc73813b0c
Loadngg <averinj@mail.ru>
2021-11-23 14:51:56 +0300

    добавлен оверлей к кнопкам: без плавного появления

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,17 @@ $(document).ready(function(){
         autoplaySpeed:2000,
     });
 });
+
+// overflow
+function showOverflow(blockId) {
+    currBlockId = blockId;
+    let block = document.getElementById(currBlockId);
+    block.classList.add('active'); 
+};
+
+function hideOverflow() {
+    let block = document.getElementById(currBlockId);
+    let iframe = block.firstElementChild.firstElementChild;
+    block.classList.remove('active');
+    iframe.src = iframe.src;
+}
